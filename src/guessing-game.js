57eced960bf72c56054ebcaa6f4b81622de19ff8
Anna <89348633+annTerry@users.noreply.github.com>
2023-02-06 20:29:00 +0300

    feat: task solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,22 @@
 class GuessingGame {
     constructor() {}
-
+    current = 0;
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
-    guess() {
-
+    guess() {        
+        this.current = Math.ceil(this.min + (this.max - this.min) / 2);        
+        return this.current;
     }
 
     lower() {
-
+        this.max = this.current;
     }
 
     greater() {
-
+        this.min = this.current;
     }
 }
 
